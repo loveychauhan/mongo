263d16f2876df7779b2133ee3479b86309c83015
Lovey Chauhan <143213952+loveychauhan@users.noreply.github.com>
2025-08-13 03:03:00 +0530

    Update cloudinary.js

--- cloudinaryPage/config/cloudinary.js
+++ cloudinaryPage/config/cloudinary.js
@@ -3,9 +3,9 @@ import { v2 as cloudinary } from 'cloudinary';
 
 async function cloudinaryUpload() {
     cloudinary.config({
-        cloud_name: 'drv6vorri',
-        api_key: '611398716875871',
-        api_secret: 'NDLLuEcDX_5N4DKk7eiqYSRXRYk'
+        cloud_name: '0000000ri',
+        api_key: '*********871',
+        api_secret: '********RYk'
     });
 }
 
